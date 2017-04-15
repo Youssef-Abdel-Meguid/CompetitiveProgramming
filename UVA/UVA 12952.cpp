99544653ebef20c775b676eca462ae9fcc25231f
Youssef-Abdel-Meguid <yousef.dev.c@gmail.com>
2017-04-15 21:54:23 +0200

    Create UVA 12952.cpp

---
+++ UVA/UVA 12952.cpp
@@ -0,0 +1,17 @@
+/*
+ Duh....
+*/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    while(cin >> a >> b)
+    {
+        cout << max(a, b) << endl;
+    }
+    return 0;
+}
